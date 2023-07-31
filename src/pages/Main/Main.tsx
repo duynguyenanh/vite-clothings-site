b1011708e3f931d81683bef26d1b389872f62937
Andy Nguyen <duy.nguyenanh1112@gmail.com>
2023-07-31 15:50:12 +1200

    config src and add styled components

---
+++ src/pages/Main/Main.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+
+import { Header } from "src/containers";
+
+const Main: FC = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default Main;
---
+++ src/res/colors.ts
@@ -0,0 +1,6 @@
+export const headerBackground = "#F6F6F7";
+export const fontNormal = "#222222";
+export const fontBold = "#888888";
+export const requiredStar = "#C90000";
+export const borderLightGrey = "#CCCCCC";
+export const borderDarkGrey = "#222222";
